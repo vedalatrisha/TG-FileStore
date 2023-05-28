4f4089ffdaf82b149ba864861b9033a1eb293526
vedalatrisha <133808711+vedalatrisha@users.noreply.github.com>
2023-05-28 23:52:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,14 +4,14 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	API_ID = int(os.environ.get("API_ID", "17193413"))
+	API_HASH = os.environ.get("5e50e6380252d9d487093bae4171bf91")
+	BOT_TOKEN = os.environ.get("6051634511:AAGF5kIFvhLFEeNJK5iJCtzKAcCVRkph27s")
+	BOT_USERNAME = os.environ.get("powerbotzz_file_store_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001980651834"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1606667548"))
+	DATABASE_URL = os.environ.get("mongodb+srv://powermovies:powermovies11@cluster0.je3b1k7.mongodb.net/?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "PowerMoviesBots")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
